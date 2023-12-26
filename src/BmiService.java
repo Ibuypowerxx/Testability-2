2a343107847f865cba9a781266a3cde8bf770498
ibuypower <danroz1995@yandex.ru>
2023-12-26 20:51:17 +0300

    Correct

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,9 +1,7 @@
 public class BmiService {
     public int calculate(double height, double weight) {
-        int result;
-        height = 1.64; // Рост
-        weight = 50; // Вес
         double bmi = weight / (height * height); // Индекс массы тела
+        int result;
         result = (int) bmi;
         return result;
     }
